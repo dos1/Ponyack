ed19459b391ea5529dd103de9d157050cf7fc84c
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-10-06 03:01:58 +0200

    add page background

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 define(['libs/jquery'], function() {
-  $('#loading').fadeOut(1000);
-  $('h1, #content').fadeIn(1000);
+  $(window).on('load', function() {
+    $('#loading').fadeOut(1000);
+    $('h1, #content').fadeIn(1000);
+  });
 });
