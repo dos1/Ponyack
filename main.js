614e5918ab2c1aa6585cb420ccd94f8a5c737220
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-10-06 03:26:10 +0200

    fun with fonts

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 define(['libs/jquery'], function() {
   $(window).on('load', function() {
     $('#loading').fadeOut(1000);
-    $('h1, #content').fadeIn(1000);
+    $('#wrapper').fadeIn(1000);
   });
 });
