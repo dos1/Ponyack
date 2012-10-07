18fa8446ed704a9bfc73b0509bbacee6ca7224c9
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-10-07 02:56:46 +0200

    sessions!

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.bodyParser());
+app.use(express.cookieParser('tajemne haslo ponyacka'));
+app.use(express.session());
 
 app.get('/', function(req, res) {
   res.send('Hello World');
@@ -9,15 +11,23 @@ app.get('/', function(req, res) {
 
 app.post('/login', function(req, res) {
   console.log("Login try:", req.param('login'));
-  if (req.param('login') === "dos") {
+  if (req.param('login') === "Pirat") {
     res.send(JSON.stringify({status:'NOK'}));
   } else {
-    res.send(JSON.stringify({status:'OK', login:req.param('login'), hasCharacter: false}));
+    req.session.login = req.param('login');
+    res.send(JSON.stringify({status:'OK', login:req.session.login, hasCharacter: req.session.hasCharacter }));
   }
 });
 
 app.get('/login', function(req, res) {
-  res.send(JSON.stringify({login:''}));
+  var login = '';
+  if (req.session) { login=req.session.login; }
+  res.send(JSON.stringify({login:login}));
+});
+
+app.get('/logout', function(req, res) {
+  req.session.destroy();
+  res.send('');
 });
 
 app.listen(8910);
